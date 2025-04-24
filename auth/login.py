782678e4398f1064a7cff9a4bdd9a567391953a7
Vitor <vitormeiragomes@outlook.com>
2025-04-24 16:44:42 -0300

    ajustando email

--- auth/login.py
+++ auth/login.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
-#//===================================================================//
-# Verifica se o email existe no DF                                    //
-#//===================================================================//
+# Verifica se o email existe no DF
 
 def autenticar_usuario(email: str, df_holerite: pd.DataFrame) -> bool:
     return email in df_holerite["Email"].values
