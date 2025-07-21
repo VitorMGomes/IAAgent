56265fe3eb4610858dddcc4b2b79c75762ca7678
Vitor de Meira Gomes <vitormeiragomes@outlook.com>
2025-07-21 18:57:26 -0300

    Criando pasta UI

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ api_process = subprocess.Popen(["uvicorn", "api:app", "--reload"])
 
 time.sleep(2)
 
-streamlit_process = subprocess.Popen(["streamlit", "run", "Home.py"])
+streamlit_process = subprocess.Popen(["streamlit", "run", "ui/Home.py"])
 
 api_process.wait()
 streamlit_process.wait()
